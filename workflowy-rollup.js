06e06fd70cd4af3d9ca2f0cec5cd81e7bd91f964
Mike Matz <mike@pixor.net>
2013-02-06 07:52:52 -0800

    Add float support

--- workflowy-rollup.js
+++ workflowy-rollup.js
@@ -1,18 +1,21 @@
 (function () {
-    var valueFromString = function (str) {
-        var r = str.match(/\[(\d+)\]$/);
-        if (r) {
-            return parseInt(r[1], 10);
-        } else {
-            return 0;
+    var valueFromString = function (s) {
+        var pats = [/\[(\d+\.?\d*)\]$/, /\((\d+\.?\d*)\)$/, /\s(\d+\.?\d*)$/];
+        for (var k in pats) {
+            var pat = pats[k];
+            var r = s.match(pat);
+            if (r) {
+                return parseFloat(r[1]);
+            }
         }
+        return 0.0;
     };
     var nodeValue = function (el) {
         var value = $(el).attr('data-node-value');
         if (value === undefined) {
             var name = $(el).children('.name').children('.content').text();
             var myVal = valueFromString(name);
-            var childVals = 0;
+            var childVals = 0.0;
             $(el).children('.children').children('.project').each(function (idx, childEl) {
                 childVals += nodeValue(childEl);
             });
@@ -24,7 +27,7 @@
     var addRollups = function () {
         $('.project').each(function (idx, el) {
             val = nodeValue(el);
-            if (val) {
+            if (val != 0) {
                 $(el).addClass('js-changed-name');
                 $name = $(el).children('.name').children('.content');
                 $(el).attr('data-original-name', $name.text());
