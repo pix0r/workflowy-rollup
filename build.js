186d0daee58b444c0f656d1210446ada7efaec21
Mike Matz <mike@pixor.net>
2013-02-06 08:35:32 -0800

    Add bookmarklet to README and add to build process

--- build.js
+++ build.js
@@ -1,20 +1,36 @@
+#!/usr/bin/env node
+var argv = require('optimist').argv;
 var fs = require('fs');
 var bookmarkletify = require('bookmarkletify');
 var mu = require('mu2');
 
 var scriptFile = 'workflowy-rollup.js';
-var templateFile = 'demo.mustache';
-var outputFile = 'demo.html';
+
+var parseFiles = {
+	'templates/demo.mustache': 'demo.html',
+	'templates/README.mustache': 'README.md',
+};
 
 var content = fs.readFileSync(scriptFile, 'utf-8');
 var bookmarkletString = bookmarkletify(content);
 
-var outStream = mu.compileAndRender(templateFile, {'bookmarklet': bookmarkletString});
-var outString = "";
-outStream.on('data', function(data) {
-	outString += data;
-});
-outStream.on('end', function() {
-	fs.writeFileSync(outputFile, outString, 'utf-8');
-	console.log("Updated " + outputFile);
-});
+if (argv.v) {
+	console.log(bookmarkletString);
+}
+
+var ctx = {'bookmarklet': bookmarkletString};
+var outputBuffers = {};
+var inFile;
+var renderFile = function(inFile, outFile, ctx) {
+	var buf = "";
+	var stream = mu.compileAndRender(inFile, ctx);
+	stream.on('data', function(data) { buf += data; });
+	stream.on('end', function() {
+		fs.writeFileSync(outFile, buf, 'utf-8');
+	});
+};
+for (inFile in parseFiles) {
+	if (parseFiles.hasOwnProperty(inFile)) {
+		renderFile(inFile, parseFiles[inFile], ctx);
+	}
+}
